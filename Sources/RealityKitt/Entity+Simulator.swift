20b9e75086996ecc186949376316ce6b96ce9973
Lennart Stolz <lennartstolz@me.com>
2021-06-29 21:00:14 +0200

    Enhanced the simulator interface of 'Entity'

--- Sources/RealityKitt/Entity+Simulator.swift
+++ Sources/RealityKitt/Entity+Simulator.swift
@@ -6,11 +6,25 @@ import RealityKit
 @available(iOS 13.0, *)
 extension RealityKit.Entity {
 
+    /// Simulator interface mock of `Entity.loadBodyTracked(named:in:)`.
+    ///
+    /// The (real) type method isn't exposed to simulators by `RealityKit`.
+    ///
+    /// - SeeAlso:
+    /// [ Documentation](https://developer.apple.com/documentation/realitykit/entity/3255555-loadbodytracked)
     public static func loadBodyTracked(named name: Swift.String,
                                        in bundle: Foundation.Bundle? = nil) throws -> BodyTrackedEntity {
         BodyTrackedEntity()
     }
 
+    /// Simulator interface mock of `Entity.loadBodyTrackedAsync(named:in:)`.
+    ///
+    /// The (real) type method isn't exposed to simulators by `RealityKit`.
+    ///
+    /// - Warning: Calling this method will throw a `fatalError`.
+    ///
+    /// - SeeAlso:
+    /// [ Documentation](https://developer.apple.com/documentation/realitykit/entity/3255557-loadbodytrackedasync)
     public static func loadBodyTrackedAsync(named name: Swift.String,
                                             in bundle: Foundation.Bundle? = nil)
     -> RealityKit.LoadRequest<BodyTrackedEntity> {
@@ -19,11 +33,25 @@ extension RealityKit.Entity {
         fatalError("loadBodyTrackedAsync(named:in:) has not been implemented")
     }
 
+    /// Simulator interface mock of `Entity.loadBodyTracked(contentsOf:withName:)`.
+    ///
+    /// The (real) type method isn't exposed to simulators by `RealityKit`.
+    ///
+    /// - SeeAlso:
+    /// [ Documentation](https://developer.apple.com/documentation/realitykit/entity/3255554-loadbodytracked)
     public static func loadBodyTracked(contentsOf url: Foundation.URL,
                                        withName resourceName: Swift.String? = nil) throws -> BodyTrackedEntity {
         BodyTrackedEntity()
     }
 
+    /// Simulator interface mock of `Entity.loadBodyTrackedAsync(contentsOf:withName:)`.
+    ///
+    /// The (real) type method isn't exposed to simulators by `RealityKit`.
+    ///
+    /// - Warning: Calling this method will throw a `fatalError`.
+    ///
+    /// - SeeAlso:
+    /// [ Documentation](https://developer.apple.com/documentation/realitykit/entity/3255556-loadbodytrackedasync)
     public static func loadBodyTrackedAsync(contentsOf url: Foundation.URL,
                                             withName resourceName: Swift.String? = nil)
     -> RealityKit.LoadRequest<BodyTrackedEntity> {
