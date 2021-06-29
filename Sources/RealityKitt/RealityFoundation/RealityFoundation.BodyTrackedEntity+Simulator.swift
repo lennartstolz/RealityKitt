70023883192d1eff08b78f1e61d9b23b417f217e
Lennart Stolz <lennartstolz@me.com>
2021-06-29 20:43:32 +0200

    Enhanced the simulator interface  of 'BodyTrackedEntity'

--- Sources/RealityKitt/RealityFoundation/RealityFoundation.BodyTrackedEntity+Simulator.swift
+++
@@ -1,8 +0,0 @@
-#if targetEnvironment(simulator)
-
-import RealityKit
-
-@available(iOS 13.0, *)
-public class BodyTrackedEntity : RealityKit.Entity, HasBodyTracking, RealityKit.HasModel { }
-
-#endif
